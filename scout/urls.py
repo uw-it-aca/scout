582cad6e9a9fc8379aa9f8ef2fdae3b631f917a6
Charlon Palacay <charlon@gmail.com>
2015-04-23 15:20:17 -0700

    Updates url sceme.

--- scout/urls.py
+++ scout/urls.py
@@ -12,14 +12,14 @@ urlpatterns = patterns('',
     
     # application urls
     
+    url(r'^$', 'scout.views.home_view', name='home_view'),
+    
     url(r'^map/', 'scout.views.map_view', name='map_view'),
     url(r'^list/', 'scout.views.list_view', name='list_view'),
     
-    url(r'^detail/', 'scout.views.detail_view', name='detail_view'),
+    url(r'^detail/\d{1,2}', 'scout.views.detail_view', name='detail_view'),
       
     url(r'^favorites/', 'scout.views.favorites_view', name='favorites_view'),
     url(r'^filters/', 'scout.views.filters_view', name='filters_view'),
     
-    url(r'^$', 'scout.views.home_view', name='home_view'),
-     
 )
