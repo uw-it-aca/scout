a879025ccbb2f61064a43ef6b92dfeb6978a9432
Craig M. Stimmel <cstimmel@uw.edu>
2015-09-28 14:54:49 -0700

    Make scout installable by adding setup.py.

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='scout',
+    version='0.1',
+    packages=['scout'],
+    include_package_data=True,
+    install_requires = [
+        'setuptools',
+        'django',
+        'django-compressor',
+        'django_mobileesp',
+        'django-turbolinks',
+        'django-htmlmin',
+        'libsass',
+    ],
+    license='Apache License, Version 2.0',  # example license
+    description='A Django app for developer resources complimentary to mdot_web client.',
+    long_description=README,
+    url='http://www.example.com/',
+    author='Your Name',
+    author_email='yourname@example.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
