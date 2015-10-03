07dd6da199127be11c28eb757bc5ae3675a05690
Charlon Palacay <charlon@gmail.com>
2015-10-02 22:21:13 -0700

    Adds support for material ripple library.

--- scout/static/hybrid/js/hybrid.js
+++ scout/static/hybrid/js/hybrid.js
@@ -1,3 +1,17 @@
+// Initialize your app
+
+
 $(function() {
     console.log( "ready!" );
+    
+    
+    var myApp = new Framework7({
+		material: true,
+	    materialRipple: true,
+	    materialRippleElements: '.ripple-blue, .ripple-green',
+	    fastClicks: true,
+	    activeState: true,
+	    activeStateElements: 'a, button, label, span',
+	});
+    
 });
