5384d08f0f726f4292e9784c6e17422d9d9dcf37
Charlon Palacay <charlon@gmail.com>
2015-10-08 10:09:56 -0700

    Proper google maps initialization using turbolinks.

--- scout/static/scout/js/web.js
+++ scout/static/scout/js/web.js
@@ -1,23 +1,13 @@
 
 // initial page load
 
-$( document).ready(function() {
-    
+$(function () { 
+	    
     // async load css by flipping the media attribute to all
     $('link[rel="stylesheet"]').attr('media', 'all');
     
     //Turbolinks.ProgressBar.start();
     
     console.log("blajfdlksjalaskdfjfasd");
-
-    
-});
-
-
-// page loaded via turbolinks
-
-$(document).on('page:load', function(event) {
-  
- 	console.log("this was loaded on turbolinks page load");
-  
+ 
 });
