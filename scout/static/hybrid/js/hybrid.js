9f53539c5c19b7d2d0fc40a3fdd3c340ed929c2a
Charlon Palacay <charlon@gmail.com>
2015-10-16 10:28:32 -0700

    Adds javascript bridge example to filter page.

---
+++ scout/static/hybrid/js/hybrid.js
@@ -0,0 +1,11 @@
+document.addEventListener('WebViewJavascriptBridgeReady', function(event) {
+    
+    // initialize bridge
+    var bridge = event.bridge
+    bridge.init()
+
+    // Start using the bridge by registering events
+    
+    
+        
+}, false)
