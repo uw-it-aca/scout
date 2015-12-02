6bc8f5fbffd7af24ced1d6fc0a16bf223cc7cbb8
Stephen De Vight <devights@gmail.com>
2015-12-01 17:33:47 -0800

    wiring up details template

--- scout/views.py
+++ scout/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from scout.space_dao import get_open_spots
+from scout.space_dao import get_open_spots, get_spot_by_id
 
 import urllib
 import json
@@ -25,8 +25,10 @@ def list_view(request):
     context = {"spots": spots}
     return render_to_response('scout/list.html', context, context_instance=RequestContext(request))
 
-def detail_view(request):
-    return render_to_response('scout/detail.html', context_instance=RequestContext(request))
+def detail_view(request, spot_id):
+    spot = get_spot_by_id(spot_id)
+    context = {"spot": spot}
+    return render_to_response('scout/detail.html', context, context_instance=RequestContext(request))
 
 def hybrid_comps_view(request):
     return render_to_response('hybridize/components.html', context_instance=RequestContext(request))
