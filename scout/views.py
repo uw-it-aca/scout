125dacc8b2bca483116585abf971f78689a32d91
Stephen De Vight <devights@gmail.com>
2015-12-01 16:29:06 -0800

    wire up list

--- scout/views.py
+++ scout/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from scout.space_dao import get_open_spots
 
 import urllib
 import json
@@ -9,7 +10,7 @@ import json
 
 def discover_view(request):
     return render_to_response('scout/discover.html', context_instance=RequestContext(request))
-    
+
 def map_view(request):
     return render_to_response('scout/map.html', context_instance=RequestContext(request))
 
@@ -18,9 +19,11 @@ def filter_view(request):
 
 def favorites_view(request):
     return render_to_response('scout/favorites.html', context_instance=RequestContext(request))
-        
+
 def list_view(request):
-    return render_to_response('scout/list.html', context_instance=RequestContext(request))
+    spots = get_open_spots()
+    context = {"spots": spots}
+    return render_to_response('scout/list.html', context, context_instance=RequestContext(request))
 
 def detail_view(request):
     return render_to_response('scout/detail.html', context_instance=RequestContext(request))
