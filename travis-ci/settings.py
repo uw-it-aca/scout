23b8c4ae927d8638e5562b45efbedae88f21759d
Stephen De Vight <devights@gmail.com>
2015-12-11 15:41:25 -0800

    removign scout manager

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scout',
-    'scout_manager',
     'hybridize',
     'turbolinks',
     'spotseeker_restclient',
