c92bd524a6f0840808a7b98e2d3c88e46877ad0b
Charlon Palacay <charlon@gmail.com>
2015-12-11 11:27:22 -0800

    Adds filter toggle for list and map views.

--- scout/static/scout/js/web.js
+++ scout/static/scout/js/web.js
@@ -1,24 +1,14 @@
+$(document).on('ready page:load page:restore', function(event) {
 
-// initial page load
-
-$(function () { 
-   	
-   	console.log("styles on")
-   	
-	// async load css by flipping the media attribute to all
+	/// async load css by flipping the media attribute to all
+    console.log("styles on")
 	$('link[rel="stylesheet"]').attr('media', 'all');
-		
 
-	/**
-	console.log("load google maps");
-	var s = document.createElement("script");
-	s.type = "text/javascript";
-	s.src = "https://maps.googleapis.com/maps/api/js";
-	// Use any selector
-	$("head").append(s);
-	**/
-	console.log("blajfdlksjalaskdfjfasd");
-	
+    $("#filter_toggle").click(function(e) {
+        e.preventDefault();
+        $("#filter_container").toggle("slow", function() {
+            // Animation complete.
+        });
+    });
+
 });
-
-
