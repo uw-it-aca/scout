be0b4585ca6ad03f4a3c34102b37f2fd88a4d977
Charlon Palacay <charlon@gmail.com>
2015-12-16 09:45:57 -0800

    Adds scrollto function.

--- scout/static/scout/js/web.js
+++ scout/static/scout/js/web.js
@@ -10,3 +10,31 @@ $(document).on('ready page:load page:restore', function(event) {
     });
 
 });
+
+// scrollTo function
+
+$.fn.scrollTo = function(target, options, callback) {
+    if (typeof options == 'function' && arguments.length == 2) {
+        callback = options;
+        options = target;
+    }
+    var settings = $.extend({
+        scrollTarget: target,
+        offsetTop: 100,
+        duration: 500,
+        //easing: 'swing'
+    }, options);
+
+    return this.each(function() {
+        var scrollPane = $(this);
+        var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+        var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+        scrollPane.animate({
+            scrollTop: scrollY
+        }, parseInt(settings.duration), settings.easing, function() {
+            if (typeof callback == 'function') {
+                callback.call(this);
+            }
+        });
+    });
+}
