ad7e82fe61b02635924c1de1ae9a7ae1efc26b81
Stephen De Vight <devights@gmail.com>
2015-12-17 18:15:21 -0800

    fleshing out discover page

--- scout/views.py
+++ scout/views.py
@@ -2,13 +2,38 @@ from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from scout.space_dao import get_open_spots, get_spot_by_id, get_filtered_spots
+from scout.space_dao import get_spots_by_filter
 
 import urllib
 import json
 
+#using drumheller fountain as the default center
+DEFAULT_LAT = 47.653717
+DEFAULT_LON = -122.307755
+
 
 def discover_view(request):
+    context = {
+        "open": get_spots_by_filter([('limit', 5),
+                                     ('open', True),
+                                     ('center_latitude', DEFAULT_LAT),
+                                     ('center_longitude', DEFAULT_LON),
+                                     ('distance', 1000),]),
+        "coffee": get_spots_by_filter([('extended_info:s_food_espresso', 'true'),
+                                       ('limit', 5),
+                                       ('open', True),
+                                       ('center_latitude', DEFAULT_LAT),
+                                       ('center_longitude', DEFAULT_LON),
+                                       ('distance', 1000),]),
+        "coupon": get_spots_by_filter([('extended_info:s_has_coupon',
+                                        'true'),
+                                       ('limit', 5),
+                                       ('center_latitude', DEFAULT_LAT),
+                                       ('center_longitude', DEFAULT_LON),
+                                       ('distance', 1000),])
+    }
     return render_to_response('scout/discover.html',
+                              context,
                               context_instance=RequestContext(request))
 
 
