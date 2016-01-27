35f33a45e947e484b0b7ee49aad745dacefc9a10
Charlon Palacay <charlon@gmail.com>
2016-01-27 10:43:25 -0800

    Adds template filter for phone formatting.

--- scout/templatetags/scout_filters.py
+++ scout/templatetags/scout_filters.py
@@ -5,3 +5,9 @@ register = template.Library()
 @register.filter
 def remove_underscores(value):
     return value.replace('_', ' ')
+
+
+@register.filter
+def format_phone(value):
+    phone = '(%s) %s - %s' % (value[0:3], value[3:6], value[6:10])
+    return phone
