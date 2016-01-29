a049f064e91f3e78924ebf9f225cd5c54b3935e3
Charlon Palacay <charlon@gmail.com>
2016-01-29 10:54:40 -0800

    Fixes registered tradmark display on ios hybrid.

--- scout/templatetags/scout_filters.py
+++ scout/templatetags/scout_filters.py
@@ -11,3 +11,15 @@ def remove_underscores(value):
 def format_phone(value):
     phone = '(%s) %s-%s' % (value[0:3], value[3:6], value[6:10])
     return phone
+
+
+@register.filter
+def stackerize(value):
+
+    # convert & to &amp;
+    value = value.replace('&', '%26')
+
+    # conver whitespace to +
+    value = value.replace(' ', '+')
+
+    return value
