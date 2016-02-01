f4aea19c805dc1604201a377344c0bfdd87d5f65
Charlon Palacay <charlon@gmail.com>
2016-02-01 15:34:41 -0800

    Initial commit of geolocation.

--- scout/static/scout/js/map.js
+++ scout/static/scout/js/map.js
@@ -6,8 +6,52 @@ $(document).on('ready page:load page:restore', function(event) {
 
 	if($("#list_map").length > 0) {
 
+        var pos;
+
 		console.log("loading list map");
-		initializeListMap();
+        // HTML5 geolocation
+        if (navigator.geolocation) {
+
+            navigator.geolocation.getCurrentPosition(function(position) {
+
+                console.log("User shared their location!")
+
+                // user's current location
+                pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+            console.log(pos);
+            initializeListMap(pos);
+
+        }, function() {
+            console.log("User did not share their location!")
+
+            // seattle fountain
+            pos = {
+                lat: 47.653811,
+                lng: -122.307815
+            };
+
+            // center map on seattle fountain
+            initializeListMap(pos);
+
+        });
+        } else {
+
+            console.log("Browser doens't support geolocation!")
+
+            // seattle fountain
+            pos = {
+                lat: 47.653811,
+                lng: -122.307815
+            };
+
+            // center map on seattle fountain
+            initializeListMap(pos);
+        }
+
 	}
 
 	// detail page map
