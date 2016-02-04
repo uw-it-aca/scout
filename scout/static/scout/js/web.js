fa455a3933dbe302010d355d18d85756f3d982ef
Charlon Palacay <charlon@gmail.com>
2016-02-04 15:28:15 -0800

    Rearranging filter link location and filter page structure.

--- scout/static/scout/js/web.js
+++ scout/static/scout/js/web.js
@@ -89,12 +89,14 @@ var Navigation = {
             $("#link_discover").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
         }
         else if (pathname.indexOf("/filter") >= 0) {
-            $("#link_filter").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
+            //$("#link_filter").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
+            $("#link_discover").css("border-bottom", "solid 3px #fff");
+            $("#link_all").css("border-bottom", "solid 3px #fff");
         }
         else if (pathname.indexOf("/detail") >= 0) {
             $("#link_discover").css("border-bottom", "solid 3px #fff");
             $("#link_all").css("border-bottom", "solid 3px #fff");
-            $("#link_filter").css("border-bottom", "solid 3px #fff");
+            //$("#link_filter").css("border-bottom", "solid 3px #fff");
         }
         else {
             $("#link_all").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
