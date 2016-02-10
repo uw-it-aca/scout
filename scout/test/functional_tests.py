a7f76221b373d6b7756058a4c15472d4ff32b8a8
Charlon Palacay <charlon@gmail.com>
2016-02-10 15:18:56 -0800

    Basic functional test using selenium.

--- scout/test/functional_tests.py
+++ scout/test/functional_tests.py
@@ -40,6 +40,7 @@ SKIP_TEXT = 'Functional tests are disabled'
 
 class ExampleTestCase(LiveServerTestCase):
 	def setUp(self):
+
 		# start display
 		self.vdisplay = Display(visible=0, size=(1024, 768))
 		self.vdisplay.start()
@@ -58,11 +59,25 @@ class ExampleTestCase(LiveServerTestCase):
 		self.vdisplay.stop()
 
 	# check if this test should be skipped
-	@skipIf(SKIP_TESTS, SKIP_TEXT)
+	#@skipIf(SKIP_TESTS, SKIP_TEXT)
 	def test_example(self):
 		# run tests
 		self.selenium.get(
-			'%s%s' % (self.live_server_url, '/test/example/')
+			'%s%s' % (self.live_server_url, '/')
 		)
 		test = self.selenium.find_element_by_id('test')
 		self.assertEqual(test.text,"Hello World!")
+
+    #User can browse all spaces on campus for a place to eat without knowing anything about the space - https://jira.cac.washington.edu/browse/SCOUT-1
+
+    #User can search for a place to eat on campus by entering the name of the place as a search filter - https://jira.cac.washington.edu/browse/SCOUT-2
+
+    #User can find a place to eat on campus by viewing pins on a map which represent spots that meet the criteria of "open now" and are either near the user's current location or are centered around a central point on campus. https://jira.cac.washington.edu/browse/SCOUT-4
+
+    #User can find a place to eat by searching for places that are 'open specific hours' https://jira.cac.washington.edu/browse/SCOUT-5
+
+    #User can bring up a list of the most popular places to eat by clicking on a link in the app https://jira.cac.washington.edu/browse/SCOUT-6
+
+    #User can bring up a list of the places that serve coffee based on their current location by clicking a link https://jira.cac.washington.edu/browse/SCOUT-7
+
+    #User can bring up a list of places that serve breakfast by clicking a link in the app
--- scout/tests.py
+++ scout/tests.py
@@ -1,3 +1,4 @@
 from django.utils import unittest
 
 from scout.test.space_dao import SpaceDAOTest
+#from scout.test.functional_tests import ExampleTestCase
