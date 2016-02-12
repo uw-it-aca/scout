f972a451d9a317749ec37930917b8de93a02ce8a
Charlon Palacay <charlon@gmail.com>
2016-02-12 10:26:51 -0800

    Fixes pep8 error for tests.

--- scout/tests.py
+++ scout/tests.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 from django.utils import unittest
+from scout.test.space_dao import SpaceDAOTest
 
 USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
 ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
 
 if USERNAME and ACCESS_KEY:
     from scout.test.fidelity_pageflow import PageFlowTest
-
-from scout.test.space_dao import SpaceDAOTest
