30dfc15ed1b19c7a3606b8709c14b54b1fce6d2f
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-12 09:44:47 -0800

    Only run the selenium tests if you have a saucelabs username and access key defined.

--- scout/tests.py
+++ scout/tests.py
@@ -1,4 +1,10 @@
+from django.conf import settings
 from django.utils import unittest
 
+USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
+ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
+
+if USERNAME and ACCESS_KEY:
+    from scout.test.fidelity_pageflow import PageFlowTest
+
 from scout.test.space_dao import SpaceDAOTest
-from scout.test.fidelity_pageflow import PageFlowTest
