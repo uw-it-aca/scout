c8e1e47c3cd9837f53a074a9a7e85e07044db0d8
Charlon Palacay <charlon@gmail.com>
2016-02-19 12:09:26 -0800

    Fixes filter link active style.

--- scout/static/scout/js/navigation.js
+++ scout/static/scout/js/navigation.js
@@ -9,12 +9,12 @@ var Navigation = {
             $("#link_discover").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
         }
         else if (pathname.indexOf("/filter") >= 0) {
-            $("#link_filter").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
+            $("#link_discover").css("border-bottom", "solid 3px #fafafa");
+            $("#link_food").css("border-bottom", "solid 3px #fafafa");
         }
         else if (pathname.indexOf("/detail") >= 0) {
             $("#link_discover").css("border-bottom", "solid 3px #fafafa");
             $("#link_food").css("border-bottom", "solid 3px #fafafa");
-            $("#link_filter").css("border-bottom", "solid 3px #fafafa");
         }
         else {
             $("#link_food").css({"border-bottom":"solid 3px #6564A8", "color":"#6564A8"});
