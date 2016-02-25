979c7c02a90f76c95c167560718afa826300538c
Charlon Palacay <charlon@gmail.com>
2016-02-25 14:29:57 -0800

    Fixes urls py pep8 error.

--- scout/urls.py
+++ scout/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     # application urls
-    #url(r'^discover/', 'scout.views.discover_view', name='discover_view'),
-
     url(r'^food/', 'scout.views.list_view', name='list_view'),
 
     url(r'^discover_card/(?P<discover_category>[a-zA-Z]+)/',
@@ -15,7 +13,6 @@ urlpatterns = patterns(
     url(r'^map/', 'scout.views.map_view', name='map_view'),
     url(r'^detail/(?P<spot_id>[0-9]{1,5})',
         'scout.views.detail_view', name='detail_view'),
-    #url(r'^favorites/', 'scout.views.favorites_view', name='favorites_view'),
     url(r'^filter/', 'scout.views.filter_view', name='filter_view'),
 
     # example hybrid components
