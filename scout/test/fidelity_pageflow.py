87c54fad5836aec810837c1699abbfa70f4cb53c
k24dizzle <zhaok24@uw.edu>
2016-03-09 23:00:31 +0000

    updated pageflow tests

--- scout/test/fidelity_pageflow.py
+++ scout/test/fidelity_pageflow.py
@@ -1,5 +1,6 @@
 """
 A simple functional headless UI test with pyvirtualdisplay and selenium
+Links and URLS
 """
 
 import os
@@ -23,6 +24,7 @@ class PageFlowTest(LiveServerTestCase):
             'platform': "Mac OS X 10.9",
             'browserName': "chrome",
             'version': "31",
+            'tags': ["Pageflow", "Kevin"] 
         }
 
         self.driver = webdriver.Remote(
@@ -41,12 +43,73 @@ class PageFlowTest(LiveServerTestCase):
 
     def test_main_navigation(self):
 
-        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: Test Navigation")
+        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: Navigate Path #1")
 
         self.driver.get('http://localhost:8001/')
         self.driver.find_element_by_id('link_discover').click()
         self.driver.find_element_by_id('link_food').click()
         self.driver.find_element_by_id('link_home').click()
+
+    # Travels through food - filter - home
+    def test_food(self):
+
+        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: Navigate Path #2")
+
+        self.driver.get('http://localhost:8001/')
+        self.driver.find_element_by_id('link_food').click()
+        self.driver.find_element_by_id('link_filter').click()
+        self.driver.find_element_by_id('link_home').click()
+
+    # checks to see if you are on a discover/food tab if you can click the tab (you shouldn't be able to)
+    def test_clickable(self):
+
+        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: Clickable")
+
+        self.driver.get('http://localhost:8001/')
+        temp2 = self.driver.find_element_by_id('link_food').click()
+        temp = self.driver.current_url
+        self.driver.find_element_by_id('link_food').click()
+        self.assertEqual(temp, self.driver.current_url)
+        self.driver.find_element_by_id('link_discover').click()
+        temp = self.driver.current_url
+        self.driver.find_element_by_id('link_discover').click()
+        self.assertEqual(temp, self.driver.current_url)
+
+    # goes from home - food - filter - reset - filter - search - resetFilters - details1 - localhost/food - filter 
+    # localhost/discover - home
+    def test_everything(self):
+        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: Everything")
+
+        self.driver.get('http://localhost:8001/')
+        self.driver.find_element_by_id('link_food').click()
+        self.driver.find_element_by_id('link_filter').click()
+        self.driver.find_element_by_id('reset_button').click()
+        self.driver.find_element_by_id('link_filter').click()
+        self.driver.find_element_by_id('run_search').click()
+        self.driver.find_element_by_id('reset_filter').click()
+        self.driver.find_element_by_id('1').click()
+        self.driver.find_element_by_id('link_home').click() # checking to see if home
+        self.assertEqual("http://localhost:8001/", self.driver.current_url)
+        #all pages can be reached by URL
+
+    # Sees if the following pages are reachable by URL
+    def test_URL(self):
+        sauce_client.jobs.update_job(self.driver.session_id, name="Pageflow: URL")
+        #HomePage
+        self.driver.get('http://localhost:8001/')
+        #FoodPage
+        self.driver.get('http://localhost:8001/food')
+        #Discover Page
+        self.driver.get('http://localhost:8001/discover/')
+        #Filter Page
+        self.driver.get('http://localhost:8001/filter/')
+
+        #Test some other potential url's that should be able to redirect
+        self.driver.get('http://localhost:8001/food') # hopefully should redirect... missing the last slash
+        self.driver.find_element_by_id('link_filter').click() # checking to see if on food
+        self.driver.get('http://localhost:8001/discover') # hopefully should redirect... missing the last slash
+
+
 
     def tearDown(self):
         print("https://saucelabs.com/jobs/%s \n" % self.driver.session_id)
