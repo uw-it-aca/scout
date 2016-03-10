876a72662543d52488d4560ce4da5b904c588bf1
k24dizzle <zhaok24@uw.edu>
2016-03-10 23:35:24 +0000

    removed the previous pageflow file, added the settings in tests.py

--- scout/tests.py
+++ scout/tests.py
@@ -6,5 +6,5 @@ USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
 ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
 
 if USERNAME and ACCESS_KEY:
-    from scout.test.fidelity_pageflow import PageFlowTest
+    from scout.test.pageflow_navigation import PageFlowNavigationTest
     from scout.test.fidelity_ui import UITest
