e6b9befd99c2fd73f06eed7f4d46b143a9fe87ab
haughar <haughar@uw.edu>
2016-03-23 15:57:25 -0700

    Fix incorrect url activity.

--- scout/urls.py
+++ scout/urls.py
@@ -4,14 +4,14 @@ from scout import views
 urlpatterns = patterns(
     '',
     # application urls
-    url(r'^food/', 'scout.views.list_view', name='list_view'),
+    url(r'^food/$', 'scout.views.list_view', name='list_view'),
 
     url(r'^discover_card/(?P<discover_category>[a-zA-Z]+)/',
         'scout.views.discover_card_view',
         name='discover_card_view'),
 
     url(r'^map/', 'scout.views.map_view', name='map_view'),
-    url(r'^detail/(?P<spot_id>[0-9]{1,5})',
+    url(r'^detail/(?P<spot_id>[0-9]{1,5})$',
         'scout.views.detail_view', name='detail_view'),
     url(r'^filter/', 'scout.views.filter_view', name='filter_view'),
     url(r'^images/(?P<spot_id>\d+)/image/(?P<image_id>\d+)', views.image_view),
@@ -21,5 +21,5 @@ urlpatterns = patterns(
         name='hybrid_comps_view'),
 
     # has to be last!
-    url(r'.*', 'scout.views.discover_view', name='discover_view'),
+    url(r'^$', 'scout.views.discover_view', name='discover_view'),
 )
