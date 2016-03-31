7722160b90a6b9080ad4b6178b0b029a97efd17b
Charlon Palacay <charlon@gmail.com>
2016-03-31 14:27:36 -0700

    Refactor layout js to use url.

--- scout/static/scout/js/layout.js
+++ scout/static/scout/js/layout.js
@@ -2,34 +2,30 @@ var Layout = {
 
     init_layout: function(){
 
-        // size the detail image height on detail.html
-        if($(".scout-details-container .scout-spot-image").length > 0) {
-            var aspectHeight = Math.round(( $(".scout-spot-image").width() /100)*67); //(i.e. 16:9 or 100:67)
-            $(".scout-spot-image").height(aspectHeight);
-    	}
+        var page_path = window.location.pathname;
 
-        // size the map on map.html (mobile only)
-        if($(".scout-map #list_map").length > 0) {
-            var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-filter-results-container").outerHeight() + $(".scout-footer").outerHeight());
-            $("#list_map").height($(window).outerHeight() - offsetHeight);
-        }
-
-        // size the place list container
-        if($(".scout-list-container").length > 0) {
+        if (page_path.indexOf("food") !== -1) {
             var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-geolocation").outerHeight() + $(".scout-filter-results").outerHeight() + $(".scout-footer").outerHeight());
             $(".scout-list-container").css({minHeight: $(window).outerHeight() - offsetHeight });
         }
-
-        // size the dicover card container
-        if($(".scout-discover-container").length > 0) {
+        else if (page_path.indexOf("detail") !== -1) {
+            var aspectHeight = Math.round(( $(".scout-spot-image").width() /100)*67); //(i.e. 16:9 or 100:67)
+            $(".scout-spot-image").height(aspectHeight);
+        }
+        else if (page_path.indexOf("map") !== -1) {
+            var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-filter-results-container").outerHeight() + $(".scout-footer").outerHeight());
+            $("#list_map").height($(window).outerHeight() - offsetHeight);
+        }
+        else {
             var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-geolocation").outerHeight() + $(".scout-footer").outerHeight());
             $(".scout-discover-container").css({minHeight: $(window).outerHeight() - offsetHeight });
-        }
 
-        // size the 404
-        if($("#page_404").length > 0) {
-            var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-footer").outerHeight());
-            $("#page_404").css({minHeight: $(window).outerHeight() - offsetHeight });
+            // 404 page
+            if($("#page_404").length > 0) {
+                var offsetHeight = ($(".scout-header").outerHeight() + $(".scout-footer").outerHeight());
+                $("#page_404").css({minHeight: $(window).outerHeight() - offsetHeight });
+            }
+
         }
 
     },
