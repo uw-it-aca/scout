d6031ec19fb722458329697f23593ce7341210dc
Charlon Palacay <charlon@gmail.com>
2016-04-19 11:58:00 -0700

    Fixes some url routing issues.

--- scout/urls.py
+++ scout/urls.py
@@ -6,35 +6,29 @@ from scout import views
 
 urlpatterns = patterns(
     '',
-
     # application urls
+    url(r'^$', 'scout.views.discover_view', name='discover_view'),
     url(r'^food/$', 'scout.views.list_view', name='list_view'),
-
-    url(r'^discover_card/(?P<discover_category>[a-zA-Z]+)/',
+    url(r'^discover_card/(?P<discover_category>[a-zA-Z]+)/$',
         'scout.views.discover_card_view',
         name='discover_card_view'),
-
-    url(r'^map/', 'scout.views.map_view', name='map_view'),
+    url(r'^map/$', 'scout.views.map_view', name='map_view'),
     url(r'^detail/(?P<spot_id>[0-9]{1,5})/$',
         'scout.views.detail_view', name='detail_view'),
-    url(r'^filter/', 'scout.views.filter_view', name='filter_view'),
-    url(r'^images/(?P<spot_id>\d+)/image/(?P<image_id>\d+)', views.image_view),
+    url(r'^filter/$', 'scout.views.filter_view', name='filter_view'),
+    url(r'^images/(?P<spot_id>\d+)/image/(?P<image_id>\d+)/$',
+        views.image_view),
 
     # hybrid urls
+    url(r'^h/$', 'scout.views.hybrid_discover_view',
+        name='hybrid_discover_view'),
     url(r'^h/food/$', 'scout.views.hybrid_list_view', name='hybrid_list_view'),
-
     url(r'^h/detail/(?P<spot_id>[0-9]{1,5})/$',
         'scout.views.hybrid_detail_view', name='hybrid_detail_view'),
-
-    url(r'^h/components/', 'scout.views.hybrid_comps_view',
+    url(r'^h/components/$', 'scout.views.hybrid_comps_view',
         name='hybrid_comps_view'),
-    url(r'^h/filter/', 'scout.views.hybrid_filter_view',
+    url(r'^h/filter/$', 'scout.views.hybrid_filter_view',
         name='hybrid_filter_view'),
-    url(r'^h/', 'scout.views.hybrid_discover_view',
-        name='hybrid_discover_view'),
-
-    # has to be last!
-    url(r'^$', 'scout.views.discover_view', name='discover_view'),
 
 )
 
