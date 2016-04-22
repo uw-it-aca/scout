6772236205ff043fbd00041c9a46a24c192d5ead
Matt Ventura <mattvent@uw.edu>
2016-04-22 14:24:49 -0700

    Added basic check for search results

---
+++ scout/test/results.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from django.test import LiveServerTestCase
+
+class ResultsTest(LiveServerTestCase):
+    
+    def test_default_results(self):
+
+        content = self.client.get('/food/').content
+
+        expected = [
+            '''"spot_name": 'Banh &amp; Naan, Husky Den',''',
+            '''"spot_name": 'Husky Grind at District Market',''',
+        ]
+
+        for exp in expected:
+            self.assertIn(exp, content)
