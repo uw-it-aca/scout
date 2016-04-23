e63de2430c0e031a83fa7a706cb894e0220b0f86
k24dizzle <zhaok24@uw.edu>
2016-04-22 17:41:27 -0700

    revised tests file to eliminate selenium files

--- scout/tests.py
+++ scout/tests.py
@@ -1,15 +1,6 @@
 from django.conf import settings
 from django.utils import unittest
 from scout.test.spacedao.space_dao import SpaceDAOTest
-
-
-USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
-ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
-
-if USERNAME and ACCESS_KEY:
-    from scout.test.pageflow.navigationSel import NavigationTestSelenium
-    from scout.test.ui.filter import FilterTest
-    from scout.test.wireframe.wireframe_navigation import AdvNavigationTest
-    from scout.test.pageflow.navigation import MainNavigationTest
-    from scout.test.pageflow.pageloadStatus import UrlStatusTest
-    from scout.test.pageflow.content import ContentTest
+from scout.test.pageflow.navigation import MainNavigationTest
+from scout.test.pageflow.pageloadStatus import UrlStatusTest
+from scout.test.pageflow.content import ContentTest
