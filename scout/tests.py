884708ad1e001dbb82776349435b274b1c108b42
k24dizzle <zhaok24@uw.edu>
2016-04-26 10:14:04 -0700

    reversed tests.py to try to fix travis outcome

--- scout/tests.py
+++ scout/tests.py
@@ -1,6 +1,15 @@
 from django.conf import settings
 from django.utils import unittest
 from scout.test.spacedao.space_dao import SpaceDAOTest
-from scout.test.pageflow.navigation import MainNavigationTest
-from scout.test.pageflow.pageloadStatus import UrlStatusTest
-from scout.test.pageflow.content import ContentTest
+
+
+USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
+ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
+
+if USERNAME and ACCESS_KEY:
+    from scout.test.pageflow.navigationSel import NavigationTestSelenium
+    from scout.test.ui.filter import FilterTest
+    from scout.test.wireframe.wireframe_navigation import AdvNavigationTest
+    from scout.test.pageflow.navigation import MainNavigationTest
+    from scout.test.pageflow.pageloadStatus import UrlStatusTest
+    from scout.test.pageflow.content import ContentTest
