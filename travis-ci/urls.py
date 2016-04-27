58f294932f637b81307ce804d8d459e83b25b754
Kevin <zhaok24@uw.edu>
2016-04-27 21:00:17 +0000

    reconfigured travis to point to the scout urls instead of scoutproject

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('scout.urls')),
 )
+
