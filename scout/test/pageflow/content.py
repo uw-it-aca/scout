d178df75068f031596e1fead8a23acfe665e3699
Kevin <zhaok24@uw.edu>
2016-05-09 23:01:24 +0000

    added content tests, altered tests.py to include them

---
+++ scout/test/pageflow/content.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+"""
+Tests the contents of each page, making sure the
+necessary selectors are present
+"""
+
+import bs4
+from django.test import TestCase
+
+
+class ContentTest(TestCase):
+    """Content test set for scout, (page id's)"""
+
+    def returnSoup(self, url):
+        response = self.client.get(url)
+        return bs4.BeautifulSoup(response.content, "html5lib")
+
+    def test_home_content(self):
+        """SCOUT-52 Test that the content on the home page is correct"""
+        bs = self.returnSoup('')
+        checkId = bs.select('#page_discover')
+        self.assertGreater(len(checkId), 0)
+
+    def test_food_content(self):
+        """SCOUT-53 Test that the content on the food page is correct"""
+        bs = self.returnSoup('/food/')
+        checkId = bs.select('#page_food')
+        self.assertGreater(len(checkId), 0)
+        filterButtons = bs.select('.scout-filter-results-action')
+        self.assertGreater(len(filterButtons), 0)
+
+    def test_detail_content(self):
+        """SCOUT-58 Test that the content on the detail page is correct"""
+        bs = self.returnSoup('/food/')
+        places = bs.select('ol li a')
+        # clicking the first place on the list
+        tempHref = places[0].get('href')
+        bs = self.returnSoup(tempHref)
+        spotName = bs.select('.scout-spot-name')
+        tempUrl = tempHref.split('/')
+        scoutContent = bs.select('.scout-content')
+        expUrl = 'page_' + tempUrl[len(tempUrl) - 2]
+        self.assertEqual(expUrl, scoutContent[0].get('id'))
+
+    def test_filter_content(self):
+        """SCOUT-54 Test that the content on the filter page is correct"""
+        bs = self.returnSoup('/filter/')
+        checkId = bs.select('#page_filter')
+        self.assertGreater(len(checkId), 0)
+        legends = bs.select('legend')
+        self.assertGreater(len(legends), 0)
