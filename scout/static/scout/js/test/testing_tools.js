6521863b93ae99212d1b244105af4f8ad78d99a4
Kevin <zhaok24@uw.edu>
2016-05-09 23:48:49 +0000

    export filter.js, added testing_tools for filterTests

--- scout/static/scout/js/test/testing_tools.js
+++ scout/static/scout/js/test/testing_tools.js
@@ -1,6 +1,32 @@
 var jsdom = require('jsdom');
 var jquery = require('jquery');
 
+var fakeSessionStorage = function fakeSessionStorage(initval){
+    if (initval !== undefined) {
+        this.sessionVars = initval;
+    } else {
+        this.sessionVars = {};
+    }
+};
+
+fakeSessionStorage.prototype.getItem = function(item) {
+    var result = this.sessionVars[item];
+    if (result === undefined) {
+        return null;
+    } else {
+        return result;
+    }
+};
+
+fakeSessionStorage.prototype.setItem = function(item, value) {
+    this.sessionVars[item] = value;
+};
+
+fakeSessionStorage.prototype.removeItem = function(item) {
+    // is this the best way to delte 
+    delete this.sessionVars[item];
+};
+
 var jqueryFromHtml = function jqueryFromHtml(html) {
     var doc = jsdom.jsdom(html);
     var win = doc.parentWindow;
@@ -8,4 +34,20 @@ var jqueryFromHtml = function jqueryFromHtml(html) {
     return $;
 };
 
+var fakeWindow = function fakeWindow(initHref) {
+    this.location = {};
+    if (initHref !== undefined) {
+        this.location.href = initHref;
+    } else {
+        this.location.href = "";
+    }
+    this.location.replace = function(new_loc) {
+        this.href = new_loc;
+    };
+
+};
+
+
+exports.fakeSessionStorage = fakeSessionStorage;
 exports.jqueryFromHtml = jqueryFromHtml;
+exports.fakeWindow = fakeWindow;
