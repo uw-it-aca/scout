a760a972110d91bfc17e7173b4797f72299ba9ce
Matt Ventura <mattvent@uw.edu>
2016-05-09 14:42:35 -0700

    Added issue keys directly to docstrings

--- scout/test/pageflow/page_load_status.py
+++ scout/test/pageflow/page_load_status.py
@@ -12,31 +12,31 @@ redir = 301
 
 # Each of these will be turned into a test function
 _testCases = (
-    ('Home', '/', OK),  # SCOUT-52
-    ('Food', '/food/', OK),  # SCOUT-53
-    ('Filter', '/filter/', OK),  # SCOUT-54
-    ('Good Details Page', '/detail/1/', OK),
-    ('Nonexistant Details Page', '/detail/88888/', notfound),  # SCOUT-55
-    ('Malformed Details ID', '/detail/abcdefg', notfound),  # SCOUT-55
-    ('Malformed Details ID 2', '/detail/123456/', notfound),  # SCOUT-55
-    ('Nonexistant page', '/nonexistant/', notfound),  # SCOUT-56
-    ('Filter Open', '/food/?open_now=true', OK),  # SCOUT-76
-    ('Filter Coffee', '/food/?food0=s_food_espresso', OK),  # SCOUT-77
-    ('Filter Breakfast', '/food/?period0=breakfast', OK),  # SCOUT-79
-    ('Filter Latenight', '/food/?period0=late_night', OK),  # SCOUT-78
-    ('Invalid Filter Params', '/food/?open_now=invalid', OK),  # SCOUT-156
-    ('Invalid Filter Params 2', '/food/?blah', OK),  # SCOUT-156
-    ('Invalid Filter Params 3', '/food/?blah=blah', OK),  # SCOUT-156
-    ('Invalid Food URL', '/food/404', notfound),  # SCOUT-157
-    ('Bad Filter', '/filter/404', notfound),  # SCOUT-81
-    ('Home Missing Slash', '', OK),  # SCOUT-57
-    ('Food Missing Slash', '/food', redir),  # SCOUT-57
-    ('Filter Missing Slash', '/filter', redir),  # SCOUT-57
-    ('Details Missing Slash', '/detail/1234', redir),  # SCOUT-57
+    ('Home', '/', OK, 'SCOUT-52'),
+    ('Food', '/food/', OK, 'SCOUT-53'),
+    ('Filter', '/filter/', OK, 'SCOUT-54'),
+    ('Good Details Page', '/detail/1/', OK, 'SCOUT-58'),
+    ('Nonexistant Details Page', '/detail/88888/', notfound, 'SCOUT-55'),
+    ('Malformed Details ID', '/detail/abcdefg', notfound, 'SCOUT-55'),
+    ('Malformed Details ID 2', '/detail/123456/', notfound, 'SCOUT-55'),
+    ('Nonexistant page', '/nonexistant/', notfound, 'SCOUT-56'),
+    ('Filter Open', '/food/?open_now=true', OK, 'SCOUT-76'),
+    ('Filter Coffee', '/food/?food0=s_food_espresso', OK, 'SCOUT-77'),
+    ('Filter Breakfast', '/food/?period0=breakfast', OK, 'SCOUT-79'),
+    ('Filter Latenight', '/food/?period0=late_night', OK, 'SCOUT-78'),
+    ('Invalid Filter Params', '/food/?open_now=invalid', OK, 'SCOUT-156'),
+    ('Invalid Filter Params 2', '/food/?blah', OK, 'SCOUT-156'),
+    ('Invalid Filter Params 3', '/food/?blah=blah', OK, 'SCOUT-156'),
+    ('Invalid Food URL', '/food/404', notfound, 'SCOUT-157'),
+    ('Bad Filter', '/filter/404', notfound, 'SCOUT-81'),
+    ('Home Missing Slash', '', OK, 'SCOUT-57'),
+    ('Food Missing Slash', '/food', redir, 'SCOUT-57'),
+    ('Filter Missing Slash', '/filter', redir, 'SCOUT-57'),
+    ('Details Missing Slash', '/detail/1234', redir, 'SCOUT-57'),
 )
 
 
-def _makeTestFunc(name, url, status=OK):
+def _makeTestFunc(name, url, status=OK, issue=None):
     """
     Returns a function that tests the given URL using
     assertUrlStatus.
@@ -45,7 +45,10 @@ def _makeTestFunc(name, url, status=OK):
         self.assertUrlStatus(url, status)
 
     _testFunc.__name__ = 'test_page_' + name.replace(' ', '_').lower()
-    _testFunc.__doc__ = 'Assert "%s" results in a %s' % (url, status)
+    doc = 'Assert "%s" results in a %s' % (url, status)
+    if issue is not None:
+        doc += ' (%s)' % issue
+    _testFunc.__doc__ = doc
 
     return _testFunc
 
