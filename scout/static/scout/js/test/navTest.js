055270966968d1ed8a5c03ce5855c4c5eb49e7e1
Kevin <zhaok24@uw.edu>
2016-05-13 23:23:01 +0000

    added initial navTest file

---
+++ scout/static/scout/js/test/navTest.js
@@ -0,0 +1,11 @@
+var Navigation = require('../navigation').Navigation;
+var jsdom = require('jsdom');
+var assert = require('assert');
+var jquery = require('jquery');
+var tools = require('./testing_tools');
+var fakeSess = require('./testing_tools').fakeSessionStorage;
+
+describe("Nav Tests", function() {
+    describe("Init Location Toggles", function() {
+     });
+});
