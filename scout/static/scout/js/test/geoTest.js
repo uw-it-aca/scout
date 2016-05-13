9e8e490ba8f6d5598245ef3b8a27bfb280092712
Kevin <zhaok24@uw.edu>
2016-05-13 21:20:53 +0000

    added geoTest file with necessary imports

---
+++ scout/static/scout/js/test/geoTest.js
@@ -0,0 +1,14 @@
+var Geolocation = require('../geolocation').Geolocation;
+var jsdom = require('jsdom');
+var assert = require('assert');
+var jquery = require('jquery');
+var tools = require('./testing_tools');
+
+describe("Geo Tests", function() {
+    describe("Test", function() {
+        it("should work", function() {
+            assert.equal(true, true);
+
+        });
+    });
+});
