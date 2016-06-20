129e3c4694c662539ac0901de002cd4923afd549
Charlon Palacay <charlon@gmail.com>
2016-06-20 13:20:56 -0700

    Updates mocha nav tests to reflect new nav logic.

--- scout/static/scout/js/navigation.js
+++ scout/static/scout/js/navigation.js
@@ -8,28 +8,20 @@ var Navigation = {
         if (pathname.indexOf("/food") !== -1) {
             $("#link_food").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
             $("#link_food").attr("aria-selected", "true");
-            // $("#link_food").attr("tabindex", "-1");
-            // $("#link_food").attr("disabled", "disabled");
-            Navigation.disable_clicks();
+            //Navigation.disable_clicks();
         }
         else if  (pathname.indexOf("/study") !== -1) {
             $("#link_study").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
             $("#link_study").attr("aria-selected", "true");
-            // $("#link_study").attr("tabindex", "-1");
-            // $("#link_study").attr("disabled", "disabled");
-            Navigation.disable_clicks();
+            //Navigation.disable_clicks();
         }
         else if (pathname.indexOf("/tech") !== -1) {
            $("#link_tech").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
            $("#link_tech").attr("aria-selected", "true");
-           // $("#link_tech").attr("tabindex", "-1");
-           // $("#link_tech").attr("disabled", "disabled");
-           Navigation.disable_clicks();
+           //Navigation.disable_clicks();
        }
-        else if (pathname.indexOf("/detail") !== -1) {
-            $("#link_food").css("border-bottom", "solid 4px #fff");
-            $("#link_study").css("border-bottom", "solid 4px #fff");
-            $("#link_tech").css("border-bottom", "solid 4px #fff");
+        else {
+            $("#link_home").attr("aria-selected", "true");
         }
 
     },
