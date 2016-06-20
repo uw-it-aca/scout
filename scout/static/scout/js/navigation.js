765bba1e056c1edc9163a7ca94bf6b8af8339400
Charlon Palacay <charlon@gmail.com>
2016-06-20 12:17:14 -0700

    Updates navigation clickability of items.

--- scout/static/scout/js/navigation.js
+++ scout/static/scout/js/navigation.js
@@ -8,22 +8,22 @@ var Navigation = {
         if (pathname.indexOf("/food") !== -1) {
             $("#link_food").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
             $("#link_food").attr("aria-selected", "true");
-            $("#link_food").attr("tabindex", "-1");
-            $("#link_food").attr("disabled", "disabled");
+            // $("#link_food").attr("tabindex", "-1");
+            // $("#link_food").attr("disabled", "disabled");
             Navigation.disable_clicks();
         }
         else if  (pathname.indexOf("/study") !== -1) {
             $("#link_study").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
             $("#link_study").attr("aria-selected", "true");
-            $("#link_study").attr("tabindex", "-1");
-            $("#link_study").attr("disabled", "disabled");
+            // $("#link_study").attr("tabindex", "-1");
+            // $("#link_study").attr("disabled", "disabled");
             Navigation.disable_clicks();
         }
         else if (pathname.indexOf("/tech") !== -1) {
            $("#link_tech").css({"border-bottom":"solid 4px #6564A8", "color":"#6564A8"});
            $("#link_tech").attr("aria-selected", "true");
-           $("#link_tech").attr("tabindex", "-1");
-           $("#link_tech").attr("disabled", "disabled");
+           // $("#link_tech").attr("tabindex", "-1");
+           // $("#link_tech").attr("disabled", "disabled");
            Navigation.disable_clicks();
        }
         else if (pathname.indexOf("/detail") !== -1) {
