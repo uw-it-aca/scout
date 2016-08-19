c39f1d614222457e7e747db1f209f6d78246a056
Charlon Palacay <charlon@gmail.com>
2016-08-18 22:13:47 -0700

    Refactor campus chooser tab handling.

--- scout/static/scout/js/navigation.js
+++ scout/static/scout/js/navigation.js
@@ -2,11 +2,28 @@ var Navigation = {
 
     set_campus_selection: function() {
 
+        // page based JS calls
+        var page_path = window.location.pathname;
+
         $("#campus_select_base").change(function(){
 
+            // get the campus value from select menu
             var url = $(this).val();
+
+            // add corresponding tab if not on discover
+            if (page_path.indexOf("food") !== -1) {
+                url = url + "/food";
+            }
+            else if (page_path.indexOf("study") !== -1){
+                url = url + "/study";
+            }
+            else if (page_path.indexOf("tech") !== -1){
+                url = url + "/tech";
+            }
+
             if (url) {
-                window.location = "/" + url; // redirect
+                // redirect to correct page
+                window.location = "/" + url;
             }
             return false;
 
