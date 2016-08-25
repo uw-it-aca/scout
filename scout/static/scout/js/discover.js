9f498026fd46bba9223d5d4af748ae7864793a53
Charlon Palacay <charlon@gmail.com>
2016-08-25 14:23:47 -0700

    Adds campus to open now card title.

--- scout/static/scout/js/discover.js
+++ scout/static/scout/js/discover.js
@@ -57,9 +57,11 @@ Discover = {
         Discover._add_distance_to_spots();
         Discover._sort_spots_on_cards();
 
+        var campus = Navigation.get_campus_selection();
+
         // update open near card title based on location sharing
         if (Geolocation.get_location_type() === "default") {
-            $("#open span.scout-card-title").html("Open now, Seattle Campus (center)");
+            $("#open span.scout-card-title").html("Open now, "+ campus +" Campus");
         }
         else {
             $("#open span.scout-card-title").html("Open, near you!");
