26f6cd77301471199d289bba9e0a3d401e1e6f65
abhi3 <abhi3@uw.edu>
2016-09-07 13:13:39 -0700

    SCOUT-260 fix. Infobox hovers over the correct location.

--- scout/static/scout/js/map.js
+++ scout/static/scout/js/map.js
@@ -116,12 +116,12 @@ var Map = {
                     // handle hover event for main list view
                     $('#' + data.id).hover(
                         function () {
-                            //map.setCenter(marker.getPosition());
                             infoWindow.setContent(
                                 "<div><strong>" + data.spot_name + "</strong><br>" +
                                 data.building + "</div>"
                             );
-                            infoWindow.open(map, marker);
+                            infoWindow.open(map);
+                            infoWindow.setPosition(marker.position);
                         },
                         function () {
                             infoWindow.close(map, marker);
