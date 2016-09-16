7886c3690d8bdca929f1f1df472d9a359f7413e5
Stephen De Vight <devights@gmail.com>
2016-09-15 18:43:23 -0700

    Resolves scout-340

--- scout/static/scout/js/navigation.js
+++ scout/static/scout/js/navigation.js
@@ -4,14 +4,30 @@ var Navigation = {
 
         $("#campus_select_base").change(function(){
             // get the campus value from select menu
-            var url = $(this).val() + Filter.get_current_type();
+            var new_campus = $(this).val();
+            var url = new_campus + Filter.get_current_type();
             var filter = Filter.get_filter_url(url);
             if (filter !== undefined) {
                 url = url + "?" + filter;
             }
+            Navigation.swap_campus_urls(new_campus);
             window.location= "/" + url;
         });
     },
+
+    swap_campus_urls: function (new_campus) {
+        // Handles case where user impatiently clicks links while waiting for
+        // new campus page to load. SCOUT-340
+        var anchor_ids_to_swap = ['link_home', 'link_food', 'link_study', 'link_tech'];
+
+        $(anchor_ids_to_swap).each(function (idx, val){
+            var anchor = $("#" + val);
+            var url = $(anchor).attr('href');
+            var new_url = url.replace(/seattle|tacoma|bothell/, new_campus);
+            $(anchor).attr('href', new_url);
+        });
+    },
+
 
     get_campus_selection: function () {
         var campus = $("#campus_select_base").val();
