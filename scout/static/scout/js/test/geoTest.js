e8cf9f32138fa5306c15e81cd4960ce3c7ef3c82
Ethan Turner <eturner64@gmail.com>
2016-09-20 19:36:29 +0000

    Modified tests to match new behavior

--- scout/static/scout/js/test/geoTest.js
+++ scout/static/scout/js/test/geoTest.js
@@ -12,7 +12,7 @@ describe("Geo Tests", function() {
         var forgetLocation;
         var useLocation;
         before(function() {
-            global.$ = tools.jqueryFromHtml( 
+            global.$ = tools.jqueryFromHtml(
                 '<span id="default_position" aria-hidden="false"><span>' +
                 '<span></span></span> <span><button id="use_location">' +
                 '<i aria-hidden="true"></i></button></span></span>' +
@@ -34,7 +34,7 @@ describe("Geo Tests", function() {
             assert.equal($(defaultPos).attr('aria-hidden'), 'true');
         });
         it("should store the correct using_location variable in local storage when using user's location", function() {
-            assert.equal(global.localStorage.getItem("using_location"), true);
+            assert.equal(global.localStorage.getItem("is_using_location"), true);
         });
         it("should toggle the hidden html correctly when returning to default location", function() {
             forgetLocation.click();
@@ -42,14 +42,14 @@ describe("Geo Tests", function() {
             assert.equal($(defaultPos).attr('aria-hidden'), 'false');
         });
         it("should store the correct using_location variable in local storage when switching back to default location", function() {
-            assert.equal(global.localStorage.getItem("using_location"), false);
+            assert.equal(global.localStorage.getItem("is_using_location"), false);
         });
     });
     describe("Display Location Status", function() {
         var sharedPos;
         var defaultPos;
         beforeEach(function() {
-            global.$ = tools.jqueryFromHtml( 
+            global.$ = tools.jqueryFromHtml(
                 '<span id="default_position" aria-hidden="false"><span>' +
                 '<span></span></span> <span><button id="use_location">' +
                 '<i aria-hidden="true"></i></button></span></span>' +
@@ -62,19 +62,19 @@ describe("Geo Tests", function() {
             sharedPos = $("#shared_position");
             defaultPos = $("#default_position");
         });
-        it("should correctly toggle hidden html based on a current location_type of user", function() { 
+        it("should correctly toggle hidden html based on a current location_type of user", function() {
             global.sessionStorage.setItem("location_type", "user");
             Geolocation.display_location_status();
             assert.equal($(defaultPos).attr('aria-hidden'), 'true');
             assert.equal($(sharedPos).attr('aria-hidden'), 'false');
         });
-        it("should correctly toggle hidden html based on a current location_type of default", function() { 
+        it("should correctly toggle hidden html based on a current location_type of default", function() {
             global.sessionStorage.setItem("location_type", "default");
             Geolocation.display_location_status();
             assert.equal($(defaultPos).attr('aria-hidden'), 'false');
             assert.equal($(sharedPos).attr('aria-hidden'), 'true');
         });
-        it("should correctly toggle hidden html based on a current location_type of undefined (default)", function() { 
+        it("should correctly toggle hidden html based on a current location_type of undefined (default)", function() {
             global.sessionStorage.setItem("location_type", undefined);
             Geolocation.display_location_status();
             assert.equal($(defaultPos).attr('aria-hidden'), 'false');
