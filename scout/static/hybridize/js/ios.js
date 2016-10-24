9f2f2824475e4ca93ca3a801b42148b8779ad0c5
Charlon Palacay <charlon@gmail.com>
2016-10-24 11:13:38 -0700

    Discover card template ready for js.

--- scout/static/hybridize/js/ios.js
+++ scout/static/hybridize/js/ios.js
@@ -15,37 +15,11 @@ $(document).on('turbolinks:load', function() {
 		activeState: true,
 	});
 
-    // Include filter js if on filter page
-	if(window.location.href.indexOf("/filter/") > -1){
-		$.getScript('/static/scout/js/filter.js');
-	}
-
     // handle closing notifcation banners
     $(".close-notification").click(function(e) {
         e.preventDefault();
         //alert( "Handler for .click() called." );
         myApp.closeNotification(".notification-item")
-    });
-
-    // handle food filter submit
-    /**
-    $("#food_filter_submit").click(function(e) {
-        e.preventDefault();
-        $.ajax({type: "GET",
-            url: "/h/seattle/food/",
-            success:function(result){
-                Turbolinks.clearCache();
-                Turbolinks.visit("/h/seattle/food/?period0=late_night", { action: 'advance' });
-            }
-        });
-    });
-    **/
-
-    // handle native submit click from native
-    $("#food_filter_submit").click(function(e) {
-        // 1. process the web form and generate the query param
-        // 2. call the native app and pose the query param as a "message"
-        callNativeApp();
     });
 
     // initialize slick image slider
@@ -54,8 +28,15 @@ $(document).on('turbolinks:load', function() {
         arrows: false,
     });
 
-    // testing JS bridge to ios native
 
+    // handle native submit click from native
+    $("#food_filter_submit").click(function(e) {
+        // 1. process the web form and generate the query param
+        // 2. call the native app and pose the query param as a "message"
+        callNativeApp();
+    });
+
+    // testing JS bridge to ios native
     function callNativeApp () {
         try {
             webkit.messageHandlers.foodJsBridge.postMessage("?period0=late_night")
