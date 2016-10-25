85c1e684c65e746f49f04d26032c25e921508382
Charlon Palacay <charlon@gmail.com>
2016-10-24 22:40:53 -0700

    Fixes erros. Converts from function based to class based views.

--- scout/urls.py
+++ scout/urls.py
@@ -5,90 +5,98 @@ from django.views.generic.base import RedirectView
 from scout import views
 from scout.views import DiscoverView
 from scout.views import DiscoverCardView
+
 from scout.views import FoodListView
 from scout.views import FoodDetailView
+from scout.views import FoodFilterView
+
+from scout.views import StudyListView
+from scout.views import StudyDetailView
+from scout.views import StudyFilterView
+
+from scout.views import TechListView
+from scout.views import TechDetailView
+from scout.views import TechFilterView
 
 urlpatterns = patterns(
     '',
     # home
     url(r'^$', RedirectView.as_view(url='/seattle')),
 
+    # discover
+    url(r'^(?P<campus>[^/]+)/$',
+        DiscoverView.as_view(template_name="scout/discover.html")),
+    url(r'^(?P<campus>[^/]+)/discover_card/(?P<discover_category>[a-zA-Z]+)/$',
+        DiscoverCardView.as_view(template_name="scout/discover_card.html")),
+
+    # food
+    url(r'^(?P<campus>[^/]+)/food/$',
+        FoodListView.as_view(template_name="scout/food/list.html")),
+    url(r'^(?P<campus>[^/]+)/food/(?P<spot_id>[0-9]{1,5})/$',
+        FoodDetailView.as_view(template_name="scout/food/detail.html")),
+    url(r'^(?P<campus>[^/]+)/food/filter/$',
+        FoodFilterView.as_view(template_name="scout/food/filter.html")),
+
+    # study
+    url(r'^(?P<campus>[^/]+)/study/$',
+        StudyListView.as_view(template_name="scout/study/list.html")),
+    url(r'^(?P<campus>[^/]+)/study/(?P<spot_id>[0-9]{1,5})/$',
+        StudyDetailView.as_view(template_name="scout/study/detail.html")),
+    url(r'^(?P<campus>[^/]+)/study/filter/$',
+        StudyFilterView.as_view(template_name="scout/study/filter.html")),
+
+    # tech
+    url(r'^(?P<campus>[^/]+)/tech/$',
+        TechListView.as_view(template_name="scout/tech/list.html")),
+    url(r'^(?P<campus>[^/]+)/tech/(?P<item_id>[0-9]{1,5})/$',
+        TechDetailView.as_view(template_name="scout/tech/detail.html")),
+    url(r'^(?P<campus>[^/]+)/tech/filter/$',
+        TechFilterView.as_view(template_name="scout/tech/filter.html")),
+
     # hybrid home
     url(r'^h/$', RedirectView.as_view(url='/h/seattle')),
-    #url(r'^h/(?P<campus>[^/]+)/$',
-    #    'scout.views.hybrid_discover_view', name='hybrid_discover_view'),
 
-    url(r'^h/(?P<campus>[^/]+)/$', DiscoverView.as_view(template_name="hybridize/discover.html")),
+    # hybrid discover
+    url(r'^h/(?P<campus>[^/]+)/$',
+        DiscoverView.as_view(template_name="hybridize/discover.html")),
+    url(r'^h/(?P<campus>[^/]+)/discover_card/'
+        '(?P<discover_category>[a-zA-Z]+)/$',
+        DiscoverCardView.as_view(
+            template_name="hybridize/discover_card.html")),
 
     # hybrid food
-    url(r'^h/(?P<campus>[^/]+)/food/$', FoodListView.as_view(template_name="hybridize/food/list.html")),
-
-    url(r'^h/(?P<campus>[^/]+)/food/(?P<spot_id>[0-9]{1,5})/$', FoodDetailView.as_view(template_name="hybridize/food/detail.html")),
-
+    url(r'^h/(?P<campus>[^/]+)/food/$',
+        FoodListView.as_view(template_name="hybridize/food/list.html")),
+    url(r'^h/(?P<campus>[^/]+)/food/(?P<spot_id>[0-9]{1,5})/$',
+        FoodDetailView.as_view(template_name="hybridize/food/detail.html")),
     url(r'^h/(?P<campus>[^/]+)/food/filter/$',
-        'scout.views.hybrid_food_filter_view', name='hybrid_food_filter_view'),
+        FoodFilterView.as_view(template_name="hybridize/food/filter.html")),
 
     # hybrid study
     url(r'^h/(?P<campus>[^/]+)/study/$',
-        'scout.views.hybrid_study_list_view', name='hybrid_study_list_view'),
-    url(r'^h/(?P<campus>[^/]+)/study/'
-        '(?P<spot_id>[0-9]{1,5})/$', 'scout.views.hybrid_study_detail_view',
-        name='hybrid_study_detail_view'),
+        StudyListView.as_view(template_name="hybridize/study/list.html")),
+    url(r'^h/(?P<campus>[^/]+)/study/(?P<spot_id>[0-9]{1,5})/$',
+        StudyDetailView.as_view(template_name="hybridize/study/detail.html")),
     url(r'^h/(?P<campus>[^/]+)/study/filter/$',
-        'scout.views.hybrid_study_filter_view',
-        name='hybrid_study_filter_view'),
+        StudyFilterView.as_view(template_name="hybridize/study/filter.html")),
 
     # hybrid tech
     url(r'^h/(?P<campus>[^/]+)/tech/$',
-        'scout.views.hybrid_tech_list_view', name='hybrid_tech_list_view'),
-    url(r'^h/(?P<campus>[^/]+)/tech/'
-        '(?P<item_id>[0-9]{1,5})/$', 'scout.views.hybrid_tech_detail_view',
-        name='hybrid_tech_detail_view'),
+        TechListView.as_view(template_name="scout/tech/list.html")),
+    url(r'^h/(?P<campus>[^/]+)/tech/(?P<item_id>[0-9]{1,5})/$',
+        TechDetailView.as_view(template_name="scout/tech/detail.html")),
     url(r'^h/(?P<campus>[^/]+)/tech/filter/$',
-        'scout.views.hybrid_tech_filter_view',
-        name='hybrid_tech_filter_view'),
+        TechFilterView.as_view(template_name="scout/tech/filter.html")),
 
     # hybrid components
     url(r'^h/components/$', 'scout.views.hybrid_comps_view',
         name='hybrid_comps_view'),
-
-    # campus discovery
-    url(r'^(?P<campus>[^/]+)/$', DiscoverView.as_view(template_name="scout/discover.html")),
-
-    # food
-    url(r'^(?P<campus>[^/]+)/food/$', FoodListView.as_view(template_name="scout/food/list.html")),
-
-    url(r'^(?P<campus>[^/]+)/food/', FoodDetailView.as_view(template_name="scout/food/detail.html")),
-
-    url(r'^(?P<campus>[^/]+)/food/filter/$',
-        'scout.views.food_filter_view', name='food_filter_view'),
-
-    # study
-    url(r'^(?P<campus>[^/]+)/study/$',
-        'scout.views.study_list_view', name='study_list_view'),
-    url(r'^(?P<campus>[^/]+)/study/'
-        '(?P<spot_id>[0-9]{1,5})/$', 'scout.views.study_detail_view',
-        name='study_detail_view'),
-    url(r'^(?P<campus>[^/]+)/study/filter/$',
-        'scout.views.study_filter_view', name='study_filter_view'),
-
-    # technology
-    url(r'^(?P<campus>[^/]+)/tech/$',
-        'scout.views.tech_list_view', name='tech_list_view'),
-    url(r'^(?P<campus>[^/]+)/tech/'
-        '(?P<item_id>[0-9]{1,5})/$', 'scout.views.tech_detail_view',
-        name='tech_detail_view'),
-    url(r'^(?P<campus>[^/]+)/tech/filter/$',
-        'scout.views.tech_filter_view', name='tech_filter_view'),
 
     # images
     url(r'^images/(?P<spot_id>\d+)/image/(?P<image_id>\d+)/$',
         views.spot_image_view),
     url(r'^item/images/(?P<item_id>\d+)/image/(?P<image_id>\d+)/$',
         views.item_image_view),
-
-    # discover card template
-    url(r'^(?P<campus>[^/]+)/discover_card/(?P<discover_category>[a-zA-Z]+)/$', DiscoverCardView.as_view(template_name="scout/discover_card.html")),
 
 )
 
