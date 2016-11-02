24c6d8f4d684ff4e9727c306665fe4f360d044c0
Charlon Palacay <charlon@gmail.com>
2016-11-02 11:41:22 -0700

    Adds geolocation functionality.

---
+++ scout/static/hybridize/js/geolocation.js
@@ -0,0 +1,9 @@
+var Geolocation = {
+
+    update_location: function (lat, lng) {
+
+        $("#user_location").html(lat + "&nbsp;&nbsp;" + lng )
+
+    },
+
+};
--- scout/static/hybridize/js/ios.js
+++ scout/static/hybridize/js/ios.js
@@ -8,6 +8,8 @@ $(document).on('turbolinks:load', function() {
     /// async load css by flipping the media attribute to all
     $('link[rel="stylesheet"]').attr('media', 'all');
 
+    //Geolocation.update_location();
+
     // discover cards
     Discover.init_cards();
 
