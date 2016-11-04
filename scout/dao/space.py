b63c507859b4e1d12270ef3bd3d0d69c7192ca63
Charlon Palacay <charlon@gmail.com>
2016-11-04 13:32:36 -0700

    Updates space dao entry for s_description.

--- scout/dao/space.py
+++ scout/dao/space.py
@@ -303,9 +303,6 @@ def get_is_spot_open(spot, now):
 def add_additional_info(spot):
     # global extended_info (study & food)
     spot.app_type = _get_extended_info_by_key("app_type", spot.extended_info)
-    spot.description = \
-        _get_extended_info_by_key("description",
-                                  spot.extended_info)
     spot.location_description = \
         _get_extended_info_by_key("location_description",
                                   spot.extended_info)
@@ -319,6 +316,8 @@ def add_additional_info(spot):
     spot.alert_notes = _get_extended_info_by_key("alert_notes",
                                                  spot.extended_info)
     # new extended_info (food only)
+    spot.description = \
+        _get_extended_info_by_key("s_description", spot.extended_info)
     spot.s_has_reservation = _get_extended_info_by_key("s_has_reservation",
                                                        spot.extended_info)
     spot.s_reservation_notes = _get_extended_info_by_key("s_reservation_notes",
