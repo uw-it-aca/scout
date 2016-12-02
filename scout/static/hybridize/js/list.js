f0f611f059fa8ff675ffa0835ba2756d3ebbe6f3
Charlon Palacay <charlon@gmail.com>
2016-12-02 12:37:26 -0800

    Defer list image loading after list ordering.

--- scout/static/hybridize/js/list.js
+++ scout/static/hybridize/js/list.js
@@ -13,18 +13,21 @@ var List = {
 
                 List.add_distances("scout-list-building", "data-building-lat", "data-building-lng");
                 List.order_list("scout-list-building", "scout_study_list", true);
+                List.defer_load_image();
 
             } else if (currentType.indexOf("tech") > -1)  {
 
                 List.add_additional_tech_distances();
                 List.order_list("scout-list-item", "scout_tech_list", false);
+                List.defer_load_image();
 
             } else {
 
                 List.order_list("scout-list-item", "scout_food_list", false);
+                List.defer_load_image();
             }
         });
-        
+
     },
 
     add_distances: function (className, latAttr, lngAttr) {
@@ -80,6 +83,14 @@ var List = {
             }
         });
         return spots;
+    },
+
+    defer_load_image: function() {
+        var spots = $(".image-defer");
+        $.each(spots, function(idx, item){
+            var imageUrl = $(this).data("defer-src");
+            $(this).css('background-image', 'url(' + imageUrl + ')');
+        });
     },
 
 };
