1ac599a2b60175b2890fe85eb93c5a7d92cbd787
Charlon Palacay <charlon@gmail.com>
2017-06-22 11:35:12 -0700

    Fixes GA to track with turbolinks loads.

--- scout/static/hybridize/js/android.js
+++ scout/static/hybridize/js/android.js
@@ -5,6 +5,10 @@ $(document).on('turbolinks:load', function() {
 
     console.log("turbolinks android fired!");
 
+    // track visits in google analytics
+    ga('send', 'pageview', (location.pathname + location.search));
+    console.info("Navigated to: " + location.pathname + location.search);
+
     /// async load css by flipping the media attribute to all
     $('link[rel="stylesheet"]').attr('media', 'all');
 
