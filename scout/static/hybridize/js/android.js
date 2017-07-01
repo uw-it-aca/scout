8dfe182e8858c42052d9174192b7d030f33cf888
Craig M. Stimmel <cstimmel@uw.edu>
2017-06-30 20:52:58 -0700

    Log a message to the console about GOOGLE_ANALYTICS_KEY being missing rather than raise an error.

--- scout/static/hybridize/js/android.js
+++ scout/static/hybridize/js/android.js
@@ -6,8 +6,13 @@ $(document).on('turbolinks:load', function() {
     console.log("turbolinks android fired!");
 
     // track visits in google analytics
-    ga('send', 'pageview', (location.pathname + location.search));
-    console.info("Navigated to: " + location.pathname + location.search);
+    try{
+        ga('send', 'pageview', (location.pathname + location.search));
+        console.info("Navigated to: " + location.pathname + location.search);
+    }
+    catch(e){
+        console.log("No ga function, GOOGLE_ANALYTICS_KEY may not be set.");
+    };
 
     /// async load css by flipping the media attribute to all
     $('link[rel="stylesheet"]').attr('media', 'all');
