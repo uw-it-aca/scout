7a009070b0564aaea7c821953e3eb6e0575cb304
Charlon Palacay <charlon@gmail.com>
2018-07-20 12:32:22 -0700

    Reverts view code to make discover work again on desktop.

--- scout/views.py
+++ scout/views.py
@@ -60,8 +60,8 @@ class DiscoverCardView(TemplateView):
         self.template_name = kwargs['template_name']
 
         # Will figure this out later
-        lat = self.request.GET.get('h_lat', None)
-        lon = self.request.GET.get('h_lng', None)
+        lat = self.request.GET.get('latitude', None)
+        lon = self.request.GET.get('longitude', None)
 
         # Hardcoded for food at the moment. Change it per need basis.
         discover_categories = {
