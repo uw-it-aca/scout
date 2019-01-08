d491407df5e3750f420ae5757ba12807bd7c8a4e
Charlon Palacay <charlon@gmail.com>
2019-01-08 15:14:07 -0800

    Update loading placeholder and location display styles.

--- scout/static/hybridize/js/ios.js
+++ scout/static/hybridize/js/ios.js
@@ -21,49 +21,9 @@ $(document).on('turbolinks:load', function() {
   		activeState: true,
 	  });
 
-
     // check if user location is enabled... if not init app in default state
     if (!$("body").data("user-location")) {
-
-        console.log("no user location");
-
-        $("#default_location").show();
-        $("#user_location").hide();
-
-        // get the app_type
-        var type = $("body").data("app-type")
-
-        if (type.indexOf("food") !== -1) {
-            // food
-            List.init();
-            Filter.init();
-
-        } else if (type.indexOf("study") !== -1){
-            // study
-            List.init();
-            Filter.init();
-
-            // study detail image slider
-            if ($( ".photo-gallery").length) {
-                $('.photo-gallery').not('.slick-initialized').slick({
-                    dots: true,
-                    arrows: false,
-                });
-            }
-
-        } else if (type.indexOf("tech") !== -1){
-            // tech
-            List.init();
-            Filter.init();
-        } else {
-            // discover
-            Discover.init_cards();
-        }
-
-        // filter
-        Filter.init_events();
-
+        WebView.load_app_default();
     }
-
 
 });
