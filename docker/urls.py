fcdeae03220e3fb0eff7b700ddb31efa8bf35373
erinRochfort99 <erochfor@uw.edu>
2021-03-18 12:51:57 -0700

    added prometheous directly to project

--- docker/urls.py
+++ docker/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^', include('scout.urls')),
+    url(r'^', include('django_prometheus.urls')), # add here for django compatibility
 ]
