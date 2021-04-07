50182ccb47049f8065540747f1b1489c60e46ed9
erinRochfort99 <erochfor@uw.edu>
2021-04-07 13:39:26 -0700

    loosen django version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "beautifulsoup4",
-        "django==2.1",
+        "django>=1.11,<=2.1",
         "django-appconf",
         "django_compressor",
         "django-pyscss",
