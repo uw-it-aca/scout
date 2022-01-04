db58ec8dd84c9b461c0e94f06610d9eb34b6aec4
Craig M. Stimmel <cstimmel@uw.edu>
2022-01-03 17:25:36 -0800

    Update copyright year

--- scout/dao/item.py
+++ scout/dao/item.py
@@ -1,4 +1,4 @@
-# Copyright 2021 UW-IT, University of Washington
+# Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 from scout.dao.space import get_spots_by_filter, _get_spot_filters, \
