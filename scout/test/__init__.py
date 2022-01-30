c79b27db57267edb0e9ec00a3d06d0dbcf0ee4e8
David-Strupinski <dstrupin@uw.edu>
2022-01-30 13:00:48 -0800

    fix DAO=Mock override name

--- scout/test/__init__.py
+++ scout/test/__init__.py
@@ -7,7 +7,7 @@ from django.test.utils import override_settings
 DAO = 'Mock'
 
 
-@override_settings(SPOTSEEKER_DAO_CLASS=DAO)
+@override_settings(RESTCLIENTS_SPOTSEEKER_DAO_CLASS=DAO)
 class ScoutTestCase(TestCase):
     '''Class to provide standard settings override'''
     pass
