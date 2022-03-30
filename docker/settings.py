d3c2cb4ea132ce9f8358d12e1455a3992e1b6e80
Craig M. Stimmel <cstimmel@uw.edu>
2022-03-30 13:00:42 -0700

    Use django-container logging

--- docker/settings.py
+++ docker/settings.py
@@ -13,7 +13,10 @@ MIDDLEWARE += ["django_user_agents.middleware.UserAgentMiddleware"]
 COMPRESS_ROOT = "/static/"
 
 COMPRESS_PRECOMPILERS = (
-    ("text/x-scss", "django_pyscss.compressor.DjangoScssFilter",),
+    (
+        "text/x-scss",
+        "django_pyscss.compressor.DjangoScssFilter",
+    ),
 )
 
 COMPRESS_ENABLED = os.getenv("COMPRESS_ENABLED", "True") == "True"
@@ -46,61 +49,14 @@ GOOGLE_MAPS_API = os.getenv("GOOGLE_MAPS_API", "")
 if os.getenv("ENV", "") == "localdev":
     DEBUG = True
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "standard": {
-            "format": "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
-            "datefmt": "[%Y-%m-%d %H:%M:%S]",
-        },
-    },
-    "handlers": {
-        "stdout": {
-            "class": "logging.StreamHandler",
-            "stream": sys.stdout,
-            "filters": ["stdout_stream"],
-            "formatter": "standard",
-        },
-        "stderr": {
-            "class": "logging.StreamHandler",
-            "stream": sys.stderr,
-            "filters": ["stderr_stream"],
-            "formatter": "standard",
-        },
-        "null": {"class": "logging.NullHandler", },
-    },
-    "filters": {
-        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"},
-        "stdout_stream": {
-            "()": "django.utils.log.CallbackFilter",
-            "callback": lambda record: record.levelno <= logging.WARNING,
-        },
-        "stderr_stream": {
-            "()": "django.utils.log.CallbackFilter",
-            "callback": lambda record: record.levelno >= logging.ERROR,
-        },
-    },
-    "loggers": {
-        "django.security.DisallowedHost": {
-            "handlers": ["null"],
-            "propagate": False,
-        },
-        "django.request": {
-            "handlers": ["stderr"],
-            "level": "WARNING",
-            "propagate": True,
-        },
-        "scout": {"handlers": ["stdout"], "level": "INFO", "propagate": True, },
-    },
-}
-
 # scout settings
 CAMPUS_URL_LIST = ["seattle", "tacoma", "bothell"]
 SCOUT_SHOW_NEWSSPLASH = os.getenv("SCOUT_SHOW_NEWSSPLASH") == "True"
 RESTCLIENTS_SPOTSEEKER_HOST = os.getenv("RESTCLIENTS_SPOTSEEKER_HOST", "")
 SPOTSEEKER_OAUTH_KEY = os.getenv("SPOTSEEKER_OAUTH_KEY", "")
 SPOTSEEKER_OAUTH_SECRET = os.getenv("SPOTSEEKER_OAUTH_SECRET", "")
-RESTCLIENTS_SPOTSEEKER_DAO_CLASS = os.getenv("RESTCLIENTS_SPOTSEEKER_DAO_CLASS", "Mock")
+RESTCLIENTS_SPOTSEEKER_DAO_CLASS = os.getenv(
+    "RESTCLIENTS_SPOTSEEKER_DAO_CLASS", "Mock"
+)
 OAUTH_USER = os.getenv("OAUTH_USER", "javerage")
 SCOUT_SHOW_ALT_TECH = os.getenv("SCOUT_SHOW_ALT_TECH") == "True"
