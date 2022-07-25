bd75fcadb9c85aa41dedb0cb2375f503657d6899
Craig M. Stimmel <cstimmel@uw.edu>
2022-07-25 14:54:25 -0700

    Revert "Further fixing pycodestyle"
    This reverts commit ca7d5f25ef81296eab03b532b03e3c3e7987ddbd.

--- scout/context_processors.py
+++ scout/context_processors.py
@@ -5,39 +5,57 @@ from django.conf import settings
 
 
 def google_maps(request):
-    """See if there is a Google Analytics web property id"""
-    gmaps_api_key = getattr(settings, "GOOGLE_MAPS_API", False)
-    return {"GOOGLE_MAPS_API": gmaps_api_key, "google_maps": gmaps_api_key}
+    """ See if there is a Google Analytics web property id
+    """
+    gmaps_api_key = getattr(settings, 'GOOGLE_MAPS_API', False)
+    return {
+        'GOOGLE_MAPS_API': gmaps_api_key,
+        'google_maps': gmaps_api_key
+    }
 
 
 def google_analytics(request):
-    ga_key = getattr(settings, "GOOGLE_ANALYTICS_KEY", False)
-    return {"GOOGLE_ANALYTICS_KEY": ga_key, "google_analytics": ga_key}
+    ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
+    return {
+        'GOOGLE_ANALYTICS_KEY': ga_key,
+        'google_analytics': ga_key
+    }
 
 
 def is_desktop(request):
     dk = not request.user_agent.is_mobile and not request.user_agent.is_tablet
 
-    return {"is_desktop": dk}
+    return {
+        'is_desktop': dk
+    }
 
 
 def is_hybrid(request):
-    hybridapp = request.GET.get("hybrid")
+    hybridapp = request.GET.get('hybrid')
 
-    return {"is_hybrid": hybridapp}
+    return {
+        'is_hybrid': hybridapp
+    }
 
 
 def scout_show_alt_tech(request):
-    return {"scout_show_alt_tech": settings.SCOUT_SHOW_ALT_TECH}
+    return {
+        'scout_show_alt_tech': settings.SCOUT_SHOW_ALT_TECH
+    }
 
 
 def compress_enabled(request):
-    return {"compress_enabled": settings.COMPRESS_ENABLED}
-
+    return {
+        'compress_enabled': settings.COMPRESS_ENABLED
+    }
 
 def campus_detect_url(request):
-    url = request.get_full_path()
-    for campus in settings.CAMPUS_URL_LIST:
-        if campus in url:
-            return {"campus": campus}
-    return {"campus": settings.CAMPUS_URL_LIST[0]}
+   url = request.get_full_path()
+   for campus in settings.CAMPUS_URL_LIST:
+       if campus in url:
+           return{
+               'campus': campus
+           }
+   return{
+       'campus': settings.CAMPUS_URL_LIST[0]
+   }
